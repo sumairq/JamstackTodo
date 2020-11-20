1540f2528a0b5447d62b6cf8c8838d434df7daa2
Sumair <sumairqaisar992@gmail.com>
2020-11-20 10:20:11 +0500

    identity fixes

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,14 +1,40 @@
-import React,{useEffect} from "react";
-import {Container, Heading, Button,Flex} from "theme-ui"
+import React,{useEffect, useState} from "react";
+import {Container, Heading, Button,Flex, NavLink} from "theme-ui"
 import netlifyIdentity from "netlify-identity-widget";
+import { Link } from "gatsby";
 
-export default props =>{ 
+export default props =>{
+    
+    const [user, setUser] = useState()
 
     useEffect(()=> {
         netlifyIdentity.init({})
     })
+
+    netlifyIdentity.on("login", user=>{
+        netlifyIdentity.close();
+        setUser(user)
+    
+    })
+
+    netlifyIdentity.on("logout", ()=>{
+        netlifyIdentity.close();
+        setUser()
+    })
    return (
     <Container>
+        <Flex as='nav'>
+  <NavLink as={Link} to="/" p={2}>
+    Home
+  </NavLink>
+  <NavLink as={Link} to="/app" p={2}>
+    Dashboard
+  </NavLink>
+   {user && (<NavLink  p={2}>
+    {user.user_metadata.full_name}
+  </NavLink>)}
+  
+</Flex>
         <Flex sx= {{ flexDirection: "Column", padding: 3}}>
         <Heading as="h1">
         Todo App
